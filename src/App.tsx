6ecc2f2800988c7c5deb726f9a7dd4c00c96dd6e
Daniel Silva <daniel5055@gmail.com>
2023-01-24 16:24:01 -0300

    Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { MouseEventHandler, useCallback, useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Coordinate = {
+  x: number
+  y: number
 }
 
-export default App;
+const Circle = ({ x, y }: Coordinate) => (
+  <div className="circle" style={{ top: y, left: x }} />
+)
+
+const App = () => {
+  const [items, setItems] = useState<Coordinate[]>([])
+  const [visibleItems, setVisibleItems] = useState<Coordinate[]>([])
+
+  const addCircle: MouseEventHandler<HTMLDivElement> = ({
+    clientX: x,
+    clientY: y,
+  }) => {
+    const newCoordinates = [...visibleItems, { x, y }]
+    setVisibleItems(newCoordinates)
+    setItems(newCoordinates)
+  }
+
+  const undo = useCallback(() => {
+    if (!visibleItems.length) return
+    setVisibleItems(visibleItems.slice(0, -1))
+  }, [visibleItems])
+
+  const redo = useCallback(() => {
+    if (visibleItems.length === items.length) return
+    setVisibleItems([...visibleItems, items[visibleItems.length]])
+  }, [items, visibleItems])
+
+  const keyDownHandler = useCallback(
+    ({ metaKey, altKey, key, shiftKey }: KeyboardEvent) => {
+      const shouldHandle = metaKey || altKey
+
+      if (shouldHandle && key === 'z') {
+        if (shiftKey) {
+          redo()
+        } else {
+          undo()
+        }
+      }
+    },
+    [redo, undo]
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyDownHandler)
+    return () => document.removeEventListener('keydown', keyDownHandler)
+  }, [keyDownHandler])
+
+  return (
+    <>
+      <div className="circle-wrapper" onMouseDown={addCircle}>
+        {visibleItems.map(({ x, y }, i) => (
+          <Circle x={x} y={y} key={`${x}-${y}-${i}`} />
+        ))}
+      </div>
+      <div className="buttons-wrapper">
+        <button onMouseDown={undo}>Undo</button>
+        <button onMouseDown={redo}>Redo</button>
+      </div>
+    </>
+  )
+}
+
+export default App
